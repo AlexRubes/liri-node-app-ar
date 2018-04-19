ffcd87fc7c7552faa876c5bf2ddc7e4885f22eaf
Alejandra Rubalcava <arubalcava@group.on>
2018-04-19 11:55:22 -0500

    twitter and spotify work!!!!!

--- liri.js
+++ liri.js
@@ -1,7 +1,70 @@
 require("dotenv").config();
 
-var keys = require("./keys");
-console.log(keys);
+//require all APIs needed
+var Twitter = require('twitter');
+var Spotify = require('node-spotify-api');
+var keys = require("./keys.js");
 
-var spotify = new Spotify(keys.spotify);
-var client = new Twitter(keys.twitter);
+//global variables 
+var params = process.argv.slice(2);
+
+//create functions to call information from each API
+var getTweets = function () {
+    var client = new Twitter(keys.twitter);
+
+    var handle = {
+        screen_name: "Alej_NU"
+    };
+
+    client.get('statuses/user_timeline', handle, function (error, tweets, response) {
+        if (!error) {
+            for (i = 0; i < tweets.length; i++) {
+                console.log(tweets[i].text);
+                console.log(tweets[i].created_at);
+            }
+        }
+    });
+};
+
+var getJam = function () {
+    var spotify = new Spotify(keys.spotify);
+
+    spotify.search({ type: 'track', query: params[1] }, function (err, data) {
+        if (err) {
+            console.log('Error occurred: ' + err);
+            return;  //from spotify npm docs
+        }
+        else {
+            var songInfo = data.tracks.items[0];
+            var songResult = 
+            console.log(songInfo.artists[0].name)
+            console.log(songInfo.name)
+            console.log(songInfo.album.name)
+            console.log(songInfo.preview_url)
+            console.log(songResult);
+        };
+    });
+};
+
+//create events
+var userInput = process.argv[2];
+
+if (userInput === 'my-tweets') {
+    getTweets();
+};
+if (userInput === 'spotify-this-song') {
+    getJam();
+};
+
+
+// switch(params[0]) {
+//     case "my-tweets":
+//       myTweets();
+//       break;
+//     case "spotify-this-song":
+//       if(params[1]){  //if a song is put named in 4th paramater go to function
+//       getJam();
+//     } else {  //if blank call it blink 182's "whats my age again"
+//       getJam("What\'s my age again");
+//     }
+//       break;
