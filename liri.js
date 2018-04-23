fd0354dec840b6386567f576da97023ee531d195
Alejandra Rubalcava <arubalcava@group.on>
2018-04-23 14:07:15 -0500

    added log.txt and default results per command

--- liri.js
+++ liri.js
@@ -7,9 +7,6 @@ var Spotify = require('node-spotify-api');
 var request = require('request');
 var fs = require("fs");
 
-//global variables 
-var params = process.argv.slice(2);
-
 //create functions to call information from each API
 var getTweets = function () {
     var client = new Twitter(keys.twitter);
@@ -21,37 +18,57 @@ var getTweets = function () {
     client.get('statuses/user_timeline', handle, function (error, tweets, response) {
         if (!error) {
             for (i = 0; i < tweets.length; i++) {
+                var tweets = 
                 console.log(tweets[i].text);
                 console.log(tweets[i].created_at);
+
+                log(tweets);
             }
         }
     });
 };
 
-var getJam = function () {
+
+
+var getJam = function (params) {
     var spotify = new Spotify(keys.spotify);
 
-    spotify.search({ type: 'track', query: params[1] }, function (err, data) {
+    var params = process.argv.slice(3).join(" ");
+
+
+    if (!params) {
+        params = 'Levels'
+        console.log(params);
+    }
+
+    spotify.search({ type: 'track', query: params }, function (err, data) {
         if (err) {
             console.log('Error occurred: ' + err);
             return;  //from spotify npm docs
         }
         else {
             var songInfo = data.tracks.items[0];
-            //console.log(songInfo);
             var songResult =
-                console.log("Artist: " + songInfo.artists[0].name)
+            console.log("Artist: " + songInfo.artists[0].name)
             console.log("Song Name: " + songInfo.name)
             console.log("Preview Link: " + songInfo.preview_url)
             console.log("Album: " + songInfo.album.name);
-        };
+
+            log(songResult);
+        }
     });
+
 };
 
 var getFlicks = function () {
 
     // Grab or assemble the movie name and store it in a variable called "movieName"
     var movieName = process.argv.slice(3).join(" ");
+
+    if (!movieName) {
+        movieName = "mr nobody";
+        console.log("If you haven't watched Mr. Nobody, then you should. It's on Netflix!");
+    }
     //console.log(movieName);
 
     // Then run a request to the OMDB API with the movie specified
@@ -66,7 +83,7 @@ var getFlicks = function () {
         if (!error && response.statusCode === 200) {
 
             // Parse the body of the site and recover just the imdbRating
-            // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
+            var movieResults = 
             console.log("Movie Title: " + JSON.parse(body).Title);
             console.log("Release Year: " + JSON.parse(body).Year);
             console.log("IMBD Rating: " + JSON.parse(body).imdbRating);
@@ -75,6 +92,8 @@ var getFlicks = function () {
             console.log("Language: " + JSON.parse(body).Language);
             console.log("Plot: " + JSON.parse(body).Plot);
             console.log("Actors: " + JSON.parse(body).Actors);
+
+            log(movieResults);
         }
     });
 };
@@ -82,24 +101,23 @@ var getFlicks = function () {
 var getData = function () {
     fs.readFile("random.txt", "utf8", function (error, data) {
 
-        // If the code experiences any errors it will log the error to the console.
-        if (error) {
-            return console.log(error);
+        if (!error) {
+            // Then split it by commas (to make it more readable)
+            var dataArr = data.split(",");
+            getJam(dataArr[0],dataArr[1]);
+        } else {
+            // If the code experiences any errors it will log the error to the console.
+            console.log("Error occurred:" + error);
         }
 
-        // We will then print the contents of data
-        //console.log(data);
+    });
+};
 
-        // Then split it by commas (to make it more readable)
-        var dataArr = data.split(",");
-
-        // We will then re-display the content as an array for later use.
-        //console.log(dataArr);
-
-        dataArr.slice(1).forEach(function (things, index, arr) {
-            console.log(things);
-        })
-
+function log(logResults) {
+    fs.appendFile("log.txt", logResults, (error) => {
+        if (error) {
+            throw error;
+        }
     });
 };
 
@@ -108,26 +126,15 @@ var userInput = process.argv[2];
 
 if (userInput === 'my-tweets') {
     getTweets();
-};
+}
 if (userInput === 'spotify-this-song') {
     getJam();
-};
+}
 if (userInput === 'movie-this') {
     getFlicks();
-};
+}
 if (userInput === 'do-what-it-says') {
     getData();
-};
+}
 
 
-// switch(params[0]) {
-//     case "my-tweets":
-//       myTweets();
-//       break;
-//     case "spotify-this-song":
-//       if(params[1]){  //if a song is put named in 4th paramater go to function
-//       getJam();
-//     } else {  //if blank call it blink 182's "whats my age again"
-//       getJam("What\'s my age again");
-//     }
-//       break;
